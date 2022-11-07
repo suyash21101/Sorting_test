19e453c552d11424d9ed443436abd7b2406eab5a
Suyash21101 <Suyash21101>
2022-11-07 13:56:21 +0000

    change

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -14,10 +14,7 @@ import pyspark
 from pyspark.sql import SparkSession
 
 #Create SparkSession
-spark = SparkSession.builder
-                    .master("local[1]")
-                    .appName("SparkByExamples.com")
-                    .getOrCreate()
+spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
 
 # COMMAND ----------
 
