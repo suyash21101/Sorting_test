d740ac049064af677a84d5f7a398f3b0646cd077
Suyash21101 <Suyash21101>
2022-11-07 13:52:57 +0000

    added spark

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,12 +1,26 @@
 # Databricks notebook source
 # MAGIC %run "./Lib"
-from Lib import bubbleSort
+# MAGIC from Lib import bubbleSort
+
 # COMMAND ----------
 
 arr = [3, 2, 1]
 bubbleSort(arr)
 
 # COMMAND ----------
+
+# Import PySpark
+import pyspark
+from pyspark.sql import SparkSession
+
+#Create SparkSession
+spark = SparkSession.builder
+                    .master("local[1]")
+                    .appName("SparkByExamples.com")
+                    .getOrCreate()
+
+# COMMAND ----------
+
 
 df = spark.table("hive_metastore.default.sorting_ds_1_csv")
 
